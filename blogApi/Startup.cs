6526cf4a51e6513131dd99ea57fbf14b6394d8f1
Anieze Collins <chigoziecollins434@gmail.com>
2020-03-05 12:48:22 +0100

    .

--- blogApi/Startup.cs
+++ blogApi/Startup.cs
@@ -35,7 +35,7 @@ namespace blogApi
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services, IHostEnvironment hostEnvironment)
+        public void ConfigureServices(IServiceCollection services)
         {
             
             services.AddCors(options =>
