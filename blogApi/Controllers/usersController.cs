bc3f11958e25d93b1298b87f0e978f9e45f1fd93
Anieze Collins <chigoziecollins434@gmail.com>
2020-03-20 19:00:12 +0100

    Created the Api for changing user password and merged image upload and edit profile endpoint.

--- blogApi/Controllers/usersController.cs
+++ blogApi/Controllers/usersController.cs
@@ -14,6 +14,7 @@ using System.IO;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Azure.Storage.Blobs;
+using blogApi.DTOS.ReadDTO;
 
 namespace blogApi.Controllers
 {
@@ -71,10 +72,33 @@ namespace blogApi.Controllers
                 return Ok(ex.Message);
             }
         }
+        
+    [HttpPut]
+    [Route("change-profile/{UserId}")]
+    public async Task<IActionResult> ChangeUserPasswordAsync([FromRoute] int UserId, [FromBody] ChangePassword passwordInfo)
+    {
+        try
+        {
+            var user = await uow.User.GetUserByIdT(UserId);
+            if (Convert.ToInt32(user.password) != passwordInfo.OldPassword)
+            {
+                return Ok(new { success = false });
+            }
 
-        // updates the user record
-        [HttpPut("edit-profile/{id}")]
-        public async Task<IActionResult> UpdateUsersAsync([FromRoute] int id, [FromBody] UserUpdateDTO UserModel)
+            user.password = passwordInfo.NewPassword.ToString();
+            uow.User.Update(user);
+            await uow.save();
+
+            return Ok(new { success = true });
+        }
+        catch (Exception ex)
+        {
+            return Ok(ex.Message);
+        }
+    }
+    // updates the user record
+    [HttpPost("edit-profile/{userId}")]
+     public async Task<IActionResult> UpdateUsersAsync([FromRoute] int userId, [FromForm] UserUpdateDTO UserModel)
         {
             if(!ModelState.IsValid)
             {
@@ -82,27 +106,99 @@ namespace blogApi.Controllers
             }
             try
             {
-                var users = await uow.User.GetUserByIdT(id);
-                if (users != null)
+                var user = await uow.User.GetUserByIdT(userId);
+                if (user != null && UserModel.Photo != null)
                 {
+                    var file = UserModel.Photo;
+                    var maxSize = 3145728;
+                    var fileNameAndExtension = file.FileName;
+                    var fileExtension = Path.GetExtension(fileNameAndExtension);
+                    string[] allowedExtensions = { ".jpg", ".png", ".jpeg" };
 
-                    users.firstname = UserModel.Firstname;
-                    users.surname = UserModel.Surname;
-                    users.state = UserModel.State;
-                    users.gender = UserModel.Gender;
-                    users.age = UserModel.Age;
-                    users.updated_at = DateTime.Now;
-                    users.email = UserModel.Email;
-                    users.username = UserModel.Username;
-                    users.country = UserModel.Country;
+                    if (file.Length <= maxSize)
+                    {
+                        if (allowedExtensions.Contains(fileExtension))
+                        {
 
-                    uow.User.Update(users);
+                            var NewFileNameForAzure = userId + "-" + Guid.NewGuid() + fileExtension;
+                            var connectionString = "DefaultEndpointsProtocol=https;AccountName=collinsazurestorage434;AccountKey=dlvvVNb0SC5AzAwHADZTy/+2h1xLSCnXDvu0huySWH56zVQBXnuISg6Q7LRTD9AZRtqW0R9yzGqUJFYhUtoAbA==;EndpointSuffix=core.windows.net";
+                            //connect to azure storage
+                            BlobContainerClient containerClient = new BlobContainerClient(connectionString, "img");
+                            // Get a reference to a blob
+                            BlobClient blobClient = containerClient.GetBlobClient(NewFileNameForAzure);
+                            
+                            
+                                var uri = "";
+                                using (Stream filetoupload = file.OpenReadStream())
+                                {
+                                    await blobClient.UploadAsync(filetoupload);
+                                    uri = blobClient.Uri.AbsoluteUri;
+                                }
+
+                                user.img_url = uri;
+                                user.updated_at = DateTime.Now;
+                                user.firstname = UserModel.Firstname;
+                                user.surname = UserModel.Surname;
+                                user.email = UserModel.Email;
+                                user.username = UserModel.Username;
+                                user.country = UserModel.Country;
+
+                                uow.User.Update(user);
+                                await uow.save();
+
+                            var UpdatedUserInfo = new UserReadDTO
+                            {
+                                userId = user.userId,
+                                surname = user.surname,
+                                firstname = user.firstname,
+                                username = user.username,
+                                age = user.age,
+                                state = user.state,
+                                date_joined = user.created_at,
+                                country = user.country,
+                                email = user.email,
+                                gender = user.gender,
+                                img_url = user.img_url
+                            };
+
+                            return Ok(new { success = true, userData = UpdatedUserInfo });                              
+                        }
+                        return Ok(new { success = false, message = "Please only images with png and Jpg extensions are allowed" });
+                    }
+                    else
+                    {
+                        return Ok(new { success = false, message = "Please select an image less than 3 mb" });
+                    }
+                }
+                else
+                {
+                    user.updated_at = DateTime.Now;
+                    user.firstname = UserModel.Firstname;
+                    user.surname = UserModel.Surname;
+                    user.email = UserModel.Email;
+                    user.username = UserModel.Username;
+                    user.country = UserModel.Country;
+
+                    uow.User.Update(user);
                     await uow.save();
 
-                    return Ok(new { success = true, message = "Profile Data Updated Successfully" });
-                }
+                    var UpdatedUserInfo = new UserReadDTO
+                    {
+                        userId = user.userId,
+                        surname = user.surname,
+                        firstname = user.firstname,
+                        username = user.username,
+                        age = user.age,
+                        state = user.state,
+                        date_joined = user.created_at,
+                        country = user.country,
+                        email = user.email,
+                        gender = user.gender,
+                        img_url = user.img_url
+                    };
 
-                return Ok(new { success = false, Message = "Failed to update" });
+                    return Ok(new { success = true, userData = UpdatedUserInfo });
+                }  
             }
             catch (Exception ex)
             {
@@ -110,108 +206,12 @@ namespace blogApi.Controllers
             }
         }
 
-        //upload user image
-        [HttpPost]
-        [Route("upload-image/{userId}")]
-        public async Task<IActionResult> UploadUserImageAsync([FromRoute] int userId, [FromForm] FileUpload model)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
-
-            var User = await uow.User.GetUserByIdT(userId);
-
-            var file = model.Photo;
-            var maxSize = 5000000;
-            var fileNameAndExtension = file.FileName;
-            var fileExtension = Path.GetExtension(fileNameAndExtension);
-            string[] allowedExtensions = { ".jpg", ".png", ".jpeg" };
-
-            //bool isUploaded = false;
-
-            if (file.Length < maxSize)
-            {
-                if (allowedExtensions.Contains(fileExtension))
-                {
-                    
-                    //var destinationPath = Path.Combine(@"C:\Users\user\Pictures\Images\ProfilePictures", NewFileName);                    
-                    // var img_url = $"http://127.0.0.1:8887/ProfilePictures/{NewFileName}";
-
-                    var NewFileNameForAzure = userId + "-" + Guid.NewGuid() + fileExtension;
-                    var connectionString = "DefaultEndpointsProtocol=https;AccountName=collinsazurestorage434;AccountKey=dlvvVNb0SC5AzAwHADZTy/+2h1xLSCnXDvu0huySWH56zVQBXnuISg6Q7LRTD9AZRtqW0R9yzGqUJFYhUtoAbA==;EndpointSuffix=core.windows.net";
-                    //connect to azure storage
-                    BlobContainerClient containerClient = new BlobContainerClient(connectionString, "img");
-                    // Get a reference to a blob
-                    BlobClient blobClient = containerClient.GetBlobClient(NewFileNameForAzure);
-                    try
-                    {
-                        var uri = "";
-                        using (Stream filetoupload = file.OpenReadStream())
-                        {
-                            await blobClient.UploadAsync(filetoupload);
-                             uri = blobClient.Uri.AbsoluteUri;
-                        }
-
-                        User.img_url = uri;
-                        User.updated_at = DateTime.Now;
-
-                        uow.User.Update(User);
-                        await uow.save();
-
-                        return Ok(new { success = true, message = "Image Uploaded Successfully", pic = uri });
-
-                    }
-                    catch(Exception e)
-                    {
-                        return Ok(e.Message);
-                    }
-
-
-                    //using (var fileStream = new FileStream(destinationPath, FileMode.Create))
-                    //{
-                    //    await file.CopyToAsync(fileStream);
-
-                    //    isUploaded = true;
-
-                    //    if(isUploaded)
-                    //    {
-                    //        try
-                    //        {
-                    //            User.img_url = img_url;
-                    //            User.updated_at = DateTime.Now;
-
-                    //            uow.User.Update(User);
-                    //            await uow.save();
-
-                    //            return Ok(new { success = true, message = "Image Uploaded Successfully", pic = img_url });
-                    //        }
-                    //        catch(Exception ex)
-                    //        {
-                    //            return Ok(ex.Message);
-                    //        }
-
-                    //    }
-
-
-                    //}
-                }
-                return Ok(new { success = false, message = "Please only images with png and Jpg extensions are allowed" });
-            }
-            else
-            {
-                return Ok(new { success = false, message = "Please select an image less than 5 mb" });
-            }
-
-           
-
-
-        }
+    
 
     
         //DELETE: 
-        [HttpDelete("delete/{id}")]
-        public async Task<ActionResult<users>> DeleteUserAsync([FromRoute] int id)
+    [HttpDelete("delete/{id}")]
+     public async Task<ActionResult<users>> DeleteUserAsync([FromRoute] int id)
         {
             try
             {
